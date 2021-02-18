7659b367101b2e565c516dca277cf3329f788351
Savio Martin <saviomartin2007@gmail.com>
2021-02-18 16:30:42 +0530

    some files changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const getData = () => {
+    fetch("data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        setData(myJson);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>
+        {data && data.length > 0 && data.map((item) => <p>{item.about}</p>)}
+      </h1>
     </div>
   );
 };
