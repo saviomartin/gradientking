c5fb5b1ded7ab35f6e4a60ccf2a36ab15c794a1c
Savio Martin <saviomartin2007@gmail.com>
2021-02-18 17:41:54 +0530

    Delete config-overrides.js

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
-
-module.exports = function override(config, env) {
-  config.resolve.plugins = config.resolve.plugins.filter(
-    (plugin) => !(plugin instanceof ModuleScopePlugin)
-  );
-
-  return config;
-};
