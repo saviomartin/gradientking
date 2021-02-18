7465be739498099bc8c3efb90e8eb24b9e15c619
Savio Martin <saviomartin2007@gmail.com>
2021-02-18 17:42:54 +0530

    Update Gradient.js

--- src/components/Gradient.js
+++ src/components/Gradient.js
@@ -1,9 +1,39 @@
 import React from "react";
 
-const Gradient = () => {
+const Gradient = ({ gradient }) => {
   return (
-    <div>
-      <h1>Gradient</h1>
+    <div className="gradient">
+      {gradient.colors.length == 5 ? (
+        <div
+          className="gradientPriview"
+          style={{
+            background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]}, ${gradient.colors[3]}, ${gradient.colors[4]})`,
+          }}
+        ></div>
+      ) : gradient.colors.length == 4 ? (
+        <div
+          className="gradientPriview"
+          style={{
+            background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]}, ${gradient.colors[3]})`,
+          }}
+        ></div>
+      ) : gradient.colors.length == 3 ? (
+        <div
+          className="gradientPriview"
+          style={{
+            background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]})`,
+          }}
+        ></div>
+      ) : (
+        <div
+          className="gradientPriview"
+          style={{
+            background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]})`,
+          }}
+        ></div>
+      )}
+
+      <h1>{gradient.colors.length}</h1>
     </div>
   );
 };
