be1c0452e092a6c77e963540b14469116bdab982
Savio Martin <saviomartin2007@gmail.com>
2021-02-19 09:11:51 +0530

    add copy functionality

--- src/components/Gradient.js
+++ src/components/Gradient.js
@@ -1,39 +1,106 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 
 const Gradient = ({ gradient }) => {
+  const copyGradient = () => {
+    var text = "Example text to appear on clipboard";
+    navigator.clipboard.writeText(text);
+  };
   return (
     <div className="gradient">
       {gradient.colors.length == 5 ? (
-        <div
-          className="gradientPriview"
-          style={{
-            background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]}, ${gradient.colors[3]}, ${gradient.colors[4]})`,
-          }}
-        ></div>
+        <>
+          <div
+            className="gradientPriview flex"
+            style={{
+              background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]}, ${gradient.colors[3]}, ${gradient.colors[4]})`,
+            }}
+          >
+            <h4>{gradient.name}</h4>
+          </div>
+          <div className="bottom">
+            <Button
+              variant="contained"
+              onClick={() =>
+                navigator.clipboard.writeText(`
+              background: linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]}, ${gradient.colors[3]}, ${gradient.colors[4]})
+              `)
+              }
+            >
+              Copy to clipboard
+            </Button>
+          </div>
+        </>
       ) : gradient.colors.length == 4 ? (
-        <div
-          className="gradientPriview"
-          style={{
-            background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]}, ${gradient.colors[3]})`,
-          }}
-        ></div>
+        <>
+          <div
+            className="gradientPriview flex"
+            style={{
+              background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]}, ${gradient.colors[3]})`,
+            }}
+          >
+            <h4>{gradient.name}</h4>
+          </div>
+          <div className="bottom">
+            <Button
+              variant="contained"
+              onClick={() =>
+                navigator.clipboard.writeText(
+                  `background: linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]}, ${gradient.colors[3]});`
+                )
+              }
+            >
+              Copy to clipboard
+            </Button>
+          </div>
+        </>
       ) : gradient.colors.length == 3 ? (
-        <div
-          className="gradientPriview"
-          style={{
-            background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]})`,
-          }}
-        ></div>
+        <>
+          <div
+            className="gradientPriview flex"
+            style={{
+              background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]});`,
+            }}
+          >
+            <h4>{gradient.name}</h4>
+          </div>
+          <div className="bottom">
+            <Button
+              variant="contained"
+              onClick={() =>
+                navigator.clipboard.writeText(`
+              background: linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]}, ${gradient.colors[2]});
+              `)
+              }
+            >
+              Copy to clipboard
+            </Button>
+          </div>
+        </>
       ) : (
-        <div
-          className="gradientPriview"
-          style={{
-            background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]})`,
-          }}
-        ></div>
+        <>
+          <div
+            className="gradientPriview flex"
+            style={{
+              background: `linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]})`,
+            }}
+          >
+            <h4>{gradient.name}</h4>
+          </div>
+          <div className="bottom">
+            <Button
+              variant="contained"
+              onClick={() =>
+                navigator.clipboard.writeText(`
+              background: linear-gradient(to left, ${gradient.colors[0]}, ${gradient.colors[1]});
+              `)
+              }
+            >
+              Copy to clipboard
+            </Button>
+          </div>
+        </>
       )}
-
-      <h1>{gradient.colors.length}</h1>
     </div>
   );
 };
