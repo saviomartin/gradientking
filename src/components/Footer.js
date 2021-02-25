a9df1f4bc7a46fa8713ac82a58c999a14e579187
Savio Martin <saviomartin2007@gmail.com>
2021-02-25 15:50:19 +0530

    basic features completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="footer">
       <h3 className="footer">
         Made with 💖 by
-        <a href="https://github.com/saviomartin"> Savio Martin</a>
+        <a href="https://github.com/saviomartin"> Savio Martin.</a> Thanks to
+        all <Link to="/contributors">Contributors.</Link>
       </h3>
     </div>
   );
