b1e0e909d20df23fc4910ef92a3008a7540a0b04
saviomartin <saviomartin2007@gmail.com>
2021-05-17 19:03:34 +0530

    header work completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
+
+// react-router-dom
 import { Link } from "react-router-dom";
 
 // material design
 import { Button, Tooltip } from "@material-ui/core";
 
-// Icon
-import Icon from "./Icon";
+import Icon from "./Icon"; // logo icon
+
+// Icons
 import {
   BsArrowsFullscreen,
   BsBookmarkCheck,
@@ -15,9 +18,12 @@ import {
 } from "react-icons/bs";
 import { GrRotateRight } from "react-icons/gr";
 import { IoMoonOutline } from "react-icons/io5";
-import { FiGithub, FiTwitter } from "react-icons/fi";
+import { FiGithub } from "react-icons/fi";
+
+// full screen
 import screenfull from "screenfull";
-import axios from "axios";
+
+import axios from "axios"; // axios
 
 const Header = ({ dark = false }) => {
   const [isFullScreen, setIsFullScreen] = useState(false);
@@ -36,9 +42,6 @@ const Header = ({ dark = false }) => {
       });
   };
 
-  const twitterLink =
-    "https://twitter.com/intent/tweet?text=Check%20out%20loficlub.now.sh%20by%20@SavioMartin7%E2%9A%A1%EF%B8%8F%0D%0A%0AThe%20best%20place%20to%20enjoy%20Hip%20hop%20beats%20to%20Relax%20or%20Study!%20%F0%9F%8E%A7%20Give%20it%20a%20try!%20You%27ll%20love%20it!%20%F0%9F%94%A5%0D%0A%0A%23lofi%20%23chillbeats";
-
   // fetch on load once
   useEffect(() => {
     fetchStarCount();
@@ -46,12 +49,14 @@ const Header = ({ dark = false }) => {
 
   return (
     <div className="p-4 flex items-center justify-between">
-      <div className="flex items-center justify-center">
-        <Icon />
-        <h1 className="logo Raleway font-extrabold text-3xl ml-2">
-          Gradient King
-        </h1>
-      </div>
+      <Link to="/">
+        <div className="flex items-center justify-center">
+          <Icon />
+          <h1 className="logo Raleway font-extrabold text-3xl ml-2">
+            Gradient King
+          </h1>
+        </div>
+      </Link>
       <div className="flex items-center justify-center h-full">
         <Tooltip title="Generator">
           <div className="w-12 overflow-hidden flex items-center justify-center rounded-md">
@@ -139,7 +144,7 @@ const Header = ({ dark = false }) => {
             href="https://buymeacoffee.com/saviomartin"
             target="_blank"
             rel="noreferrer"
-            className="ml-1 items-center bg-[#24292E] hover:bg-[#222] rounded-md relative cursor-pointer hidden lg:flex"
+            className="ml-1 items-center bg-[#24292E] hover:bg-[#222] rounded-md relative cursor-pointer flex"
           >
             <img
               src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
