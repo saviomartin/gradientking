2c30b4ebc07dee1755274ee21f46659e64fe4923
saviomartin <saviomartin2007@gmail.com>
2021-05-17 20:50:43 +0530

    add gradient

--- src/components/Gradient.js
+++ src/components/Gradient.js
@@ -1,17 +1,27 @@
 import { Button, Tooltip } from "@material-ui/core";
 import React from "react";
 
-import { BsCode, BsTriangle } from "react-icons/bs";
+import { BsBookmarkPlus, BsCode, BsTriangle } from "react-icons/bs";
 
 const Gradient = ({ gradient }) => {
   return (
     <div className="p-4 gradient-shadow bg-white m-3 rounded-md transition duration-400 cursor-pointer text-[#222]">
       <div
-        className="h-[200px] w-[280px] rounded-md"
+        className="h-[200px] w-[280px] rounded-md flex items-start justify-end p-2"
         style={{
           background: `linear-gradient(to right, ${gradient.colors[0]}, ${gradient.colors[1]})`,
         }}
-      ></div>
+      >
+        <div className="w-10 overflow-hidden flex items-center justify-center rounded-md ml-1 frosted-nav">
+          <Tooltip title="Rotate Gradient">
+            <Button className="btn">
+              <div className="w-full h-9 flex items-center justify-center overflow-hidden">
+                <BsBookmarkPlus className="text-[1.3rem]" />
+              </div>
+            </Button>
+          </Tooltip>
+        </div>
+      </div>
       <div className="w-full flex items-center justify-between">
         <div className="flex items-center uppercase">
           <h3
@@ -29,7 +39,7 @@ const Gradient = ({ gradient }) => {
         </div>
         <div className="flex items-center mt-2">
           <Tooltip title="Rotate Gradient">
-            <div className="w-12 overflow-hidden flex items-center justify-center rounded-md border border-[#eee]">
+            <div className="w-12 overflow-hidden flex items-center justify-center rounded-md border border-[#eee] frosted">
               <Button className="btn">
                 <div className="w-full h-9 flex items-center justify-center overflow-hidden">
                   <BsCode className="text-[1.5rem]" />
@@ -38,7 +48,7 @@ const Gradient = ({ gradient }) => {
             </div>
           </Tooltip>
           <Tooltip title="Rotate Gradient">
-            <div className="w-15 overflow-hidden flex items-center justify-center rounded-md border border-[#eee] ml-1">
+            <div className="w-15 overflow-hidden flex items-center justify-center rounded-md border border-[#eee] ml-1 frosted">
               <Button className="btn">
                 <div className="w-full h-9 flex items-center justify-center overflow-hidden">
                   <BsTriangle className="text-[1rem]" />
