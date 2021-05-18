84b99b5efa32f3636a205abed1bd6226ca76aebc
saviomartin <saviomartin2007@gmail.com>
2021-05-18 20:09:45 +0530

    creating view

--- src/core/FullPage.js
+++ src/core/FullPage.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { db } from "../backend";
 
 const FullPage = () => {
+  const [gradient, setGradient] = useState([]);
+
   let params = useParams();
 
   const gradientId = params.id;
+
+  useEffect(() => {
+    const getDoc = async (id) => {
+      const snapshot = await db.collection("gradients").doc(id).get();
+      setGradient(snapshot.data());
+    };
+    getDoc(gradientId);
+  }, []);
+
   return (
-    <div>
-      <h1>Full Page {gradientId}</h1>
+    <div className="w-full h-full flex items-center justify-center">
+      <h1>{JSON.stringify(gradient)}</h1>
     </div>
   );
 };
