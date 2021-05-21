ddf795121df0e51187dff6df3af45db6d4e4df5d
saviomartin <saviomartin2007@gmail.com>
2021-05-21 15:49:01 +0530

    add download feature

--- src/core/FullPage.js
+++ src/core/FullPage.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 // material design
 import { Button, Tooltip } from "@material-ui/core";
-
+import { exportComponentAsPNG } from "react-component-export-image";
 // create toast
 import toast from "react-hot-toast";
 
@@ -25,6 +25,7 @@ import {
   BsBookmarkCheck,
   BsBookmarkPlus,
   BsCode,
+  BsDownload,
   BsHeart,
   BsHeartFill,
 } from "react-icons/bs";
@@ -34,9 +35,20 @@ const FullPage = ({ align, user, savedGradients, setSavedGradients }) => {
   const [isBookMarked, setIsBookMarked] = useState(false);
 
   let params = useParams();
+  const componentRef = useRef();
 
   // get gradient id from the params
   const gradientId = params.id;
+
+  const ComponentToPrint = forwardRef((props, ref) => (
+    <div
+      className="h-[400px] w-[800px] overflow-hidden absolute top-0 left-0 z-[-10]"
+      ref={ref}
+      style={{
+        background: `linear-gradient(to ${align}, ${gradient.colors[0]}, ${gradient.colors[1]})`,
+      }}
+    ></div>
+  ));
 
   const getDoc = async (id) => {
     const snapshot = await db.collection("gradients").doc(id).get();
@@ -136,6 +148,7 @@ const FullPage = ({ align, user, savedGradients, setSavedGradients }) => {
 
   return gradient.colors ? (
     <div className="flex items-center justify-center h-[87.5vh] w-full">
+      <ComponentToPrint ref={componentRef} />
       <div
         className="w-[85%] h-[85%] flex items-center justify-center rounded-md gradient-shadow"
         style={{
@@ -237,6 +250,25 @@ const FullPage = ({ align, user, savedGradients, setSavedGradients }) => {
                         Bookmark
                       </h3>
                     )}
+                  </div>
+                </Button>
+              </div>
+            </Tooltip>
+            <Tooltip title="Bookmark Gradient">
+              <div
+                className="overflow-hidden flex items-center justify-center rounded-md border border-[#eee] bg-gray-100 transition duration-500 hover:bg-gray-200 ml-2 dark:border-[#555] dark:bg-[#181718]"
+                onClick={() =>
+                  exportComponentAsPNG(componentRef, {
+                    fileName: "gradient.png",
+                  })
+                }
+              >
+                <Button className="btn">
+                  <div className="w-40 h-9 flex items-center justify-center overflow-hidden">
+                    <BsDownload className="text-[1.3rem] dark:text-[#fafafa]" />
+                    <h3 className="ml-1 text-lg font-semibold capitalize Raleway dark:text-[#fafafa]">
+                      Download
+                    </h3>
                   </div>
                 </Button>
               </div>
